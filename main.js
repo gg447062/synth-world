a516b36d4c376f98d93af24e6d7c3fabe3651d7a
Gabriel Gutierrez <77996586+gg447062@users.noreply.github.com>
2024-02-20 15:59:28 -0600

    add ffmpeg for converting webm to mp3

--- main.js
+++ main.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+app.use((_, res, next) => {
+  res.header('Cross-Origin-Opener-Policy', 'same-origin');
+  res.header('Cross-Origin-Embedder-Policy', 'require-corp');
+  next();
+});
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (_, res) => {
