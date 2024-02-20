eccebba2a4d64c8b5770fcd0b4fe27d9f7e37465
Gabriel Gutierrez <77996586+gg447062@users.noreply.github.com>
2024-02-20 16:00:11 -0600

    add ffmpeg conversion to recordings

--- app/Components/Recorder.js
+++ app/Components/Recorder.js
@@ -1,9 +1,13 @@
 import React, { useEffect, useState, useRef } from 'react';
 import * as Tone from 'tone';
+import { FFmpeg } from '@ffmpeg/ffmpeg';
+import { fetchFile, toBlobURL } from '@ffmpeg/util';
 
 export default function RecorderModule({ mains, play, playing }) {
   const [recorder, setRecorder] = useState(null);
   const [recording, setRecording] = useState(false);
+  const [loaded, setLoaded] = useState(false);
+  const ffmpegRef = useRef(new FFmpeg());
 
   function startRecording() {
     recorder.start();
@@ -17,11 +21,33 @@ export default function RecorderModule({ mains, play, playing }) {
     }
     setRecording(false);
     const url = URL.createObjectURL(recording);
+    const audioURL = await extractAudio(url);
+
     const a = document.createElement('a');
-    a.download = 'recording.webm';
-    a.href = url;
+    a.download = 'recording.mp3';
+    a.href = audioURL;
     a.click();
     a.remove();
+  }
+
+  async function extractAudio(url) {
+    const ffmpeg = ffmpegRef.current;
+
+    const inputName = 'input.webm';
+    const outputName = 'output.mp3';
+
+    ffmpeg.writeFile(inputName, await fetchFile(url));
+
+    await ffmpeg.exec(['-i', inputName, outputName]);
+
+    const output = await ffmpeg.readFile(outputName);
+
+    const _blob = new Blob([output.buffer]);
+    const _audioURL = URL.createObjectURL(_blob);
+
+    ffmpeg.deleteFile(inputName);
+    ffmpeg.deleteFile(outputName);
+    return _audioURL;
   }
 
   useEffect(() => {
@@ -37,11 +63,36 @@ export default function RecorderModule({ mains, play, playing }) {
     }
   }, [recorder, mains]);
 
+  useEffect(() => {
+    const load = async () => {
+      const baseURL = 'https://unpkg.com/@ffmpeg/core@0.12.6/dist/umd';
+      const ffmpeg = ffmpegRef.current;
+      if (!ffmpeg.loaded) {
+        await ffmpeg.load({
+          coreURL: await toBlobURL(
+            `${baseURL}/ffmpeg-core.js`,
+            'text/javascript'
+          ),
+          wasmURL: await toBlobURL(
+            `${baseURL}/ffmpeg-core.wasm`,
+            'application/wasm'
+          ),
+        });
+        setLoaded(true);
+      }
+    };
+    load();
+  }, []);
+
   return (
     <div className="recorder-body">
       <div id="recorder-controls">
         {!recording && (
-          <button id="record-button" onClick={startRecording}>
+          <button
+            id="record-button"
+            onClick={startRecording}
+            disabled={!loaded}
+          >
             <span className="material-symbols-rounded">
               fiber_manual_record
             </span>
